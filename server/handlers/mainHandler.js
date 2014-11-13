0f1cb9f63629fc87d1f5005d28646c8d89ad7d30
Andrew Walton <andrew@greenarrow.me>
2014-11-13 08:33:02 -0500

    Removing client code from server project.

--- server/handlers/mainHandler.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	index: function(request, reply) {
-        var userId = request.auth.credentials && request.auth.credentials.user ? request.auth.credentials.user.id : undefined,
-            firstLogin = userId ? request.auth.credentials.user.firstLogin : false;
-
-        reply.view('index', { userId: userId, firstLogin: firstLogin });
-    }
-};
-
